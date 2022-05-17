3a03d349cc4e521a28d377a29280d1590dc1be01
Alexander <alch@pop-os.localdomain>
2022-05-17 07:53:03 +0200

    added nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,11 @@
+require "alch.options"
+require "alch.keymaps"
+require "alch.plugins"
+require "alch.colorscheme"
+require "alch.cmp"
+require "alch.lsp"
+require "alch.telescope"
+require "alch.treesitter"
+require "alch.comment"
+
+require('nvim-autopairs').setup{}
