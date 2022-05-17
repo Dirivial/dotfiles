be5ba303f1d7b83ce190fd1a449eb1e9bfcb0747
Alexander <alch@pop-os.localdomain>
2022-05-17 07:18:59 +0200

    removed nvim files

--- .config/nvim/lua/alch/colorscheme.lua
+++
@@ -1,8 +0,0 @@
--- Ask for colorscheme in a nicer way using a protected call. This way no error is thrown.
-local colorscheme = "gruvbox"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
