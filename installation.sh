48a2d788c6a24bbad0568ce72294daebb390d1eb
Alchaize <Alch4ize@gmail.com>
2022-06-02 21:20:13 +0200

    added oh my zsh

--- installation.sh
+++ installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S awesome kitty neovim flameshot brave rofi discord
+sudo pacman -S awesome kitty neovim flameshot brave rofi discord zsh
 
 # AwesomeWM
 mkdir $HOME/.config/awesome
@@ -19,3 +19,5 @@ cp -r kitty/* $HOME/.config/kitty/
 
 git config --global user.name "Alchaize"
 git config --global user.email "Alch4ize@gmail.com"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
