31036792ac4fb0d5f304ee6647b4c7e46e09c0f4
Alexander <alexander.kadeby@gmail.com>
2026-05-23 23:19:26 +0200

    Add waybar & wlogout to home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -6,8 +6,13 @@
 
   programs.bash.enable = true;
   programs.tmux.enable = true;
+
+  # DE
   services.awww.enable = true;
   services.dunst.enable = true;
+  programs.waybar.enable = true;
+  programs.wlogout.enable = true;
+  
 
   programs.git = {
     enable = true;
