f8d9015dcc8b29abeca8690cd65ee959eb59a1b8
Alexander <alexander.kadeby@gmail.com>
2026-05-24 20:43:26 +0200

    Fix dunst config

--- nix/hosts/common/home.nix
+++ nix/hosts/common/home.nix
@@ -2,6 +2,7 @@
 {
 
   imports = [
+    ../../modules/desktop/dunst.nix
     ../../modules/desktop/waybar.nix
     ../../modules/terminal/alacritty.nix
   ];
@@ -20,7 +21,6 @@
 
   # DE
   services.awww.enable = true;
-  services.dunst.enable = true;
   programs.wlogout.enable = true;
   
 
