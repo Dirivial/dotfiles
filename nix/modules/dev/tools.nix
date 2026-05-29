02f96f4a33caf4b168dc235ad68b9f27567488a2
Alexander <alexander.kadeby@gmail.com>
2026-05-29 10:21:05 +0200

    Add commitizen, python and uv

--- nix/modules/dev/tools.nix
+++ nix/modules/dev/tools.nix
@@ -3,6 +3,7 @@
 {
   home.packages = with pkgs; [
     clang-tools
+    commitizen
     cpplint
     delve
     fd
@@ -13,12 +14,14 @@
     markdownlint-cli
     nil
     nixfmt
+    python3
     ripgrep
     ruff
     shellcheck
     shfmt
     stylua
     ty
+    uv
   ];
 
   programs.direnv = {
