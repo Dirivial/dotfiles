06a6f6e764f379c90fc8892534f0a8144db1e2e3
Alexander <alexander.kadeby@gmail.com>
2026-05-29 18:10:25 +0200

    Add tailscale

--- nix/hosts/laptop/configuration.nix
+++ nix/hosts/laptop/configuration.nix
@@ -8,6 +8,11 @@
   networking.firewall.allowedTCPPorts = [ 53317 ];
   networking.firewall.allowedUDPPorts = [ 53317 ];
 
+  services.tailscale = {
+    enable = true;
+    openFirewall = true;
+  };
+
   # Note: Research before changing this
   system.stateVersion = "25.11";
 }
