314cc59b28e55a1571952215524bbdc22ffae8b4
Alexander <alexander.kadeby@gmail.com>
2026-05-29 14:10:02 +0200

    Add localsend & open TCP/UDP ports for it

--- nix/hosts/laptop/configuration.nix
+++ nix/hosts/laptop/configuration.nix
@@ -5,7 +5,9 @@
     ./hardware-configuration.nix
   ];
 
+  networking.firewall.allowedTCPPorts = [ 53317 ];
+  networking.firewall.allowedUDPPorts = [ 53317 ];
+
   # Note: Research before changing this
   system.stateVersion = "25.11";
 }
-
