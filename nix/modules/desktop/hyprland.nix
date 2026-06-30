635be7053c9ef9e63c80409eba9e984f74a7b94c
Alexander <alexander.kadeby@gmail.com>
2026-06-30 22:54:33 +0200

    Enable hyprlock

--- nix/modules/desktop/hyprland.nix
+++ nix/modules/desktop/hyprland.nix
@@ -9,38 +9,42 @@ in
     description = "Device-specific Hyprland profile to load.";
   };
 
-  config.xdg.configFile = {
-    "hypr/hyprland.lua".source = ../../../.config/hypr/hyprland.lua;
-    "hypr/hypridle.conf".source = ../../../.config/hypr/hypridle.conf;
-    "hypr/hyprlock.conf".source = ../../../.config/hypr/hyprlock.conf;
-    "hypr/hyprsunset.conf".source = ../../../.config/hypr/hyprsunset.conf;
+  config = {
+    programs.hyprlock.enable = true;
 
-    "hypr/hyprland/colors.lua".source = ../../../.config/hypr/hyprland/colors.lua;
-    "hypr/hyprland/execs.lua".source = ../../../.config/hypr/hyprland/execs.lua;
-    "hypr/hyprland/keybinds.lua".source = ../../../.config/hypr/hyprland/keybinds.lua;
-    "hypr/hyprland/programs.lua".source = ../../../.config/hypr/hyprland/programs.lua;
-    "hypr/hyprland/this-computer.lua".text = ''
-      require("hyprland.per-device.${cfg.profile}")
-    '';
+    xdg.configFile = {
+      "hypr/hyprland.lua".source = ../../../.config/hypr/hyprland.lua;
+      "hypr/hypridle.conf".source = ../../../.config/hypr/hypridle.conf;
+      "hypr/hyprlock.conf".source = ../../../.config/hypr/hyprlock.conf;
+      "hypr/hyprsunset.conf".source = ../../../.config/hypr/hyprsunset.conf;
 
-    "hypr/hyprland/per-device" = {
-      source = ../../../.config/hypr/hyprland/per-device;
-      recursive = true;
-    };
+      "hypr/hyprland/colors.lua".source = ../../../.config/hypr/hyprland/colors.lua;
+      "hypr/hyprland/execs.lua".source = ../../../.config/hypr/hyprland/execs.lua;
+      "hypr/hyprland/keybinds.lua".source = ../../../.config/hypr/hyprland/keybinds.lua;
+      "hypr/hyprland/programs.lua".source = ../../../.config/hypr/hyprland/programs.lua;
+      "hypr/hyprland/this-computer.lua".text = ''
+        require("hyprland.per-device.${cfg.profile}")
+      '';
 
-    "hypr/hyprland/scripts" = {
-      source = ../../../.config/hypr/hyprland/scripts;
-      recursive = true;
-    };
+      "hypr/hyprland/per-device" = {
+        source = ../../../.config/hypr/hyprland/per-device;
+        recursive = true;
+      };
 
-    "hypr/hyprlock" = {
-      source = ../../../.config/hypr/hyprlock;
-      recursive = true;
-    };
+      "hypr/hyprland/scripts" = {
+        source = ../../../.config/hypr/hyprland/scripts;
+        recursive = true;
+      };
 
-    "hypr/hyprpaper" = {
-      source = ../../../.config/hypr/hyprpaper;
-      recursive = true;
+      "hypr/hyprlock" = {
+        source = ../../../.config/hypr/hyprlock;
+        recursive = true;
+      };
+
+      "hypr/hyprpaper" = {
+        source = ../../../.config/hypr/hyprpaper;
+        recursive = true;
+      };
     };
   };
 }
